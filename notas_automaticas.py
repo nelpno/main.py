7dff2202cc7dc033a86058f8cd8c69271ceeb62a
nelpno <nelpno@gmail.com>
2022-11-08 15:11:29 -0300

    adicao de metodo OCR para leitura da nota impressa.

--- notas_automaticas.py
+++ notas_automaticas.py
@@ -72,31 +72,31 @@ def transforme_img_str(arquivo):
     img = pytesseract.image_to_string(arquivo, lang="eng", config=r'--oem 3 --psm 7 -c tessedit_char_whitelist=0123456789,')
     return img
 
-# # Abrir o programa no início, programa fechado.
-# if achar_img_tela('icone_prosol_aberto.png') is None:
-#     imagem_clicar('icone_prosol.png')
-#     while achar_img_tela('acessar_prosol.png') is None:
-#         time.sleep(.25)
-#     imagem_clicar('acessar_prosol.png')
-#     pa.moveTo(80, 327)
-#     # abrir tela de pedidos para notas
-#     while achar_img_tela('menu_prosol_esquerda.png') is None:
-#         print(1)
-#         time.sleep(.25)
-#     imagem_clicar('movimentos_menu_esquerda.png')
-#     while achar_img_tela('pedido_vendas.png') is None:
-#         print(1)
-#         time.sleep(.25)
-#     imagem_clicar('pedido_vendas.png')
-# else:
-#     imagem_clicar('icone_prosol_aberto.png')
-#
-# # abrir nova nota
-# while achar_img_tela('novo_pedido.png') is None:
-#     print(2)
-#     time.sleep(.25)
-#
-# imagem_clicar('novo_pedido.png')
+# Abrir o programa no início, programa fechado.
+if achar_img_tela('icone_prosol_aberto.png') is None:
+    imagem_clicar('icone_prosol.png')
+    while achar_img_tela('acessar_prosol.png') is None:
+        time.sleep(.25)
+    imagem_clicar('acessar_prosol.png')
+    pa.moveTo(80, 327)
+    # abrir tela de pedidos para notas
+    while achar_img_tela('menu_prosol_esquerda.png') is None:
+        print(1)
+        time.sleep(.25)
+    imagem_clicar('movimentos_menu_esquerda.png')
+    while achar_img_tela('pedido_vendas.png') is None:
+        print(1)
+        time.sleep(.25)
+    imagem_clicar('pedido_vendas.png')
+else:
+    imagem_clicar('icone_prosol_aberto.png')
+
+# abrir nova nota
+while achar_img_tela('novo_pedido.png') is None:
+    print(2)
+    time.sleep(.25)
+
+imagem_clicar('novo_pedido.png')
 
 # inserir produtos e preço
 i = 0
@@ -110,164 +110,165 @@ quantidade_lista = df['Quantidade'].tolist()
 valor_lista = df['Valor'].tolist()
 cliente_lista = df['Cliente'].tolist()
 boleto_lista = df['Boleto'].tolist()
-#
-# while i < len(codigo_lista) and math.isnan(codigo_lista[i]) is not True:
-#     escreve('{:.0f}'.format(codigo_lista[i]))
-#     time.sleep(0.2)
-#     apertar('enter')
-#     apertar('enter')
-#     escreve('{:.0f}'.format(quantidade_lista[i]))
-#     time.sleep(0.2)
-#     apertar('tab')
-#     escreve('{}'.format(valor_lista[i]).replace('.', ','))
-#     apertar('enter')
-#     time.sleep(0.2)
-#     i += 1
-#
-# # achar cliente
-# imagem_clicar('fechar_itens_nota.png')
-# while achar_img_tela('lupa_cliente.png') is None:
-#     print(2)
-#     time.sleep(.25)
-#
-# imagem_clicar('lupa_cliente.png')
-#
-# while achar_img_tela('que_contem.png') is None:
-#     print(2)
-#     time.sleep(.25)
-#
-# imagem_clicar('que_contem.png')
-# imagem_clicar('nome_fantasia.png')
-# imagem_clicar('pesquisar_por.png')
-# time.sleep(0.25)
-#
-# escreve('{}'.format(cliente_lista[0]))
-# apertar('enter')
-# apertar('enter')
-# time.sleep(0.25)
-#
-# # fechar pedido e emitir nota
-# while achar_img_tela('fecha_pedido.png') is None:
-#     print(2)
-#     time.sleep(.25)
-# imagem_clicar('fecha_pedido.png')
-#
-# while achar_img_tela('pedido_atualizado.png') is None:
-#     print(2)
-#     time.sleep(.25)
-#
-# apertar('enter')
-#
-# while achar_img_tela('nota_feita.png') is not None:
-#     print(2)
-#     time.sleep(.25)
-#
-# imagem_clicar('gerar_nota.png')
-#
-# while achar_img_tela('logo_nfe.png') is None:
-#     print(2)
-#     time.sleep(.25)
-#
-# imagem_clicar('logo_nfe.png')
-#
-# # verificar se tem boleto
-# if math.isnan(boleto_lista[0]):
-#     # imprimi
-#     while achar_img_tela('gerar_nota_eletronica.png') is None:
-#         print(2)
-#         time.sleep(.25)
-#     imagem_clicar('gerar_nota_eletronica.png')
-#     pyautogui.doubleClick(imagem_clicar('gerar_nota_eletronica.png'))
-#     if achar_img_tela('abertura_caixa.png') is not None:
-#         apertar('tab')
-#         escreve('caixa')
-#         apertar('enter')
-#         apertar('enter')
-#         time.sleep(0.5)
-#         if achar_img_tela('pulou_caixa.png') is None:
-#             escreve('caixa')
-#             apertar('tab')
-#             imagem_clicar('selecionar_conta.png')
-#             apertar('down')
-#             imagem_clicar('abrir_caixa.png')
-#             while achar_img_tela('deu_certo_caixa.png') is None:
-#                 print(2)
-#                 time.sleep(.25)
-#             apertar('enter')
-#             apertar('enter')
-# else:
-#     # emitir nota + boleto e imprimir
-#     while achar_img_tela('pode_fazer_nota.png') is None:
-#         print(5)
-#         time.sleep(.25)
-#     imagem_clicar('plano_pagamento.png')
-#
-#     while achar_img_tela('imagem_plano_pagamento.png') is None:
-#         print(5)
-#         time.sleep(.25)
-#     imagem_clicar('limpar_pagamento.png')
-#     imagem_clicar('scroll_boleto.png')
-#     pyautogui.click(x=1260, y=540)
-#     imagem_clicar('prazo_boleto.png')
-#
-#     while achar_img_tela('botao_gerar_prazo_boleto.png') is None:
-#         print(2)
-#         time.sleep(.25)
-#
-#     if achar_img_tela('clicar_parcelas.png') is None:
-#         imagem_clicar('intervalo_prazo.png')
-#         apertar('left')
-#         apertar('left')
-#         escreve('{:.0f}'.format(boleto_lista[0]))
-#         apertar('f5')
-#         apertar('f12')
-#         while achar_img_tela('salvar_prazo_pagamento.png') is None:
-#             print(2)
-#             time.sleep(.25)
-#         imagem_clicar('salvar_prazo_pagamento.png')
-#     imagem_clicar('gerar_nota_eletronica.png')
-#     imagem_clicar('gerar_nota_eletronica.png')
-#     tempo(2)
-#     if achar_img_tela('abertura_caixa.png') is not None:
-#         apertar('tab')
-#         escreve('caixa')
-#         apertar('enter')
-#         apertar('enter')
-#         time.sleep(0.5)
-#         if achar_img_tela('pulou_caixa.png') is None:
-#             escreve('caixa')
-#             apertar('tab')
-#             imagem_clicar('selecionar_conta.png')
-#             apertar('down')
-#             apertar('enter')
-#             while achar_img_tela('abrir_caixa.png') is None:
-#                 print(2)
-#                 time.sleep(.5)
-#             imagem_clicar('abrir_caixa.png')
-#             while achar_img_tela('deu_certo_caixa.png') is None:
-#                 print(2)
-#                 time.sleep(.25)
-#             apertar('enter')
-#             apertar('enter')
-#
-# while achar_img_tela('nota_finalizada.png') is None:
-#     print(2)
-#     time.sleep(.25)
-#
-# apertar('enter')
-#
-# while achar_img_tela('nota_apareceu.png') is None:
-#     print(2)
-#     time.sleep(.25)
+
+while i < len(codigo_lista) and math.isnan(codigo_lista[i]) is not True:
+    escreve('{:.0f}'.format(codigo_lista[i]))
+    time.sleep(0.2)
+    apertar('enter')
+    apertar('enter')
+    escreve('{:.0f}'.format(quantidade_lista[i]))
+    time.sleep(0.2)
+    apertar('tab')
+    escreve('{}'.format(valor_lista[i]).replace('.', ','))
+    apertar('enter')
+    time.sleep(0.2)
+    i += 1
+
+# achar cliente
+imagem_clicar('fechar_itens_nota.png')
+while achar_img_tela('lupa_cliente.png') is None:
+    print(2)
+    time.sleep(.25)
+
+imagem_clicar('lupa_cliente.png')
+
+while achar_img_tela('que_contem.png') is None:
+    print(2)
+    time.sleep(.25)
+
+imagem_clicar('que_contem.png')
+imagem_clicar('nome_fantasia.png')
+imagem_clicar('pesquisar_por.png')
+time.sleep(0.25)
+
+escreve('{}'.format(cliente_lista[0]))
+apertar('enter')
+apertar('enter')
+time.sleep(0.25)
+
+# fechar pedido e emitir nota
+while achar_img_tela('fecha_pedido.png') is None:
+    print(2)
+    time.sleep(.25)
+imagem_clicar('fecha_pedido.png')
+
+while achar_img_tela('pedido_atualizado.png') is None:
+    print(2)
+    time.sleep(.25)
+
+apertar('enter')
+
+while achar_img_tela('nota_feita.png') is not None:
+    print(2)
+    time.sleep(.25)
+
+imagem_clicar('gerar_nota.png')
+
+while achar_img_tela('logo_nfe.png') is None:
+    print(2)
+    time.sleep(.25)
+
+imagem_clicar('logo_nfe.png')
+
+# verificar se tem boleto
+if math.isnan(boleto_lista[0]):
+    # imprimi
+    while achar_img_tela('gerar_nota_eletronica.png') is None:
+        print(2)
+        time.sleep(.25)
+    imagem_clicar('gerar_nota_eletronica.png')
+    pyautogui.doubleClick(imagem_clicar('gerar_nota_eletronica.png'))
+    if achar_img_tela('abertura_caixa.png') is not None:
+        apertar('tab')
+        escreve('caixa')
+        apertar('enter')
+        apertar('enter')
+        time.sleep(0.5)
+        if achar_img_tela('pulou_caixa.png') is None:
+            escreve('caixa')
+            apertar('tab')
+            imagem_clicar('selecionar_conta.png')
+            apertar('down')
+            imagem_clicar('abrir_caixa.png')
+            while achar_img_tela('deu_certo_caixa.png') is None:
+                print(2)
+                time.sleep(.25)
+            apertar('enter')
+            apertar('enter')
+else:
+    # emitir nota + boleto e imprimir
+    while achar_img_tela('pode_fazer_nota.png') is None:
+        print(5)
+        time.sleep(.25)
+    imagem_clicar('plano_pagamento.png')
+
+    while achar_img_tela('imagem_plano_pagamento.png') is None:
+        print(5)
+        time.sleep(.25)
+    imagem_clicar('limpar_pagamento.png')
+    imagem_clicar('scroll_boleto.png')
+    pyautogui.click(x=1260, y=540)
+    imagem_clicar('prazo_boleto.png')
+
+    while achar_img_tela('botao_gerar_prazo_boleto.png') is None:
+        print(2)
+        time.sleep(.25)
+
+    if achar_img_tela('clicar_parcelas.png') is None:
+        imagem_clicar('intervalo_prazo.png')
+        apertar('left')
+        apertar('left')
+        escreve('{:.0f}'.format(boleto_lista[0]))
+        apertar('f5')
+        apertar('f12')
+        while achar_img_tela('salvar_prazo_pagamento.png') is None:
+            print(2)
+            time.sleep(.25)
+        imagem_clicar('salvar_prazo_pagamento.png')
+    imagem_clicar('gerar_nota_eletronica.png')
+    imagem_clicar('gerar_nota_eletronica.png')
+    tempo(2)
+    if achar_img_tela('abertura_caixa.png') is not None:
+        apertar('tab')
+        escreve('caixa')
+        apertar('enter')
+        apertar('enter')
+        time.sleep(0.5)
+        if achar_img_tela('pulou_caixa.png') is None:
+            escreve('caixa')
+            apertar('tab')
+            imagem_clicar('selecionar_conta.png')
+            apertar('down')
+            apertar('enter')
+            while achar_img_tela('abrir_caixa.png') is None:
+                print(2)
+                time.sleep(.5)
+            imagem_clicar('abrir_caixa.png')
+            while achar_img_tela('deu_certo_caixa.png') is None:
+                print(2)
+                time.sleep(.25)
+            apertar('enter')
+            apertar('enter')
+
+while achar_img_tela('nota_finalizada.png') is None:
+    print(2)
+    time.sleep(.25)
+
+apertar('enter')
+
+while achar_img_tela('nota_apareceu.png') is None:
+    print('esperando aqui nota apareceu')
+    time.sleep(.25)
 
 # Print da nota e pega de Dados
+tempo(1)
 img = ImageGrab.grab((899, 381, 1653, 820))
 
-valor_img = img.crop((694, 416, 737, 432))
+valor_img = img.crop((694, 416, 736, 432))
 valor_img_conv = valor_img.convert('1')
-data_img = img.crop((24, 322, 89, 335))
-numero_nfe_img = img.crop((320, 75, 368, 89))
-nome_img = img.crop((9, 210, 92, 230))
+data_img = img.crop((21, 321, 90, 336))
+numero_nfe_img = img.crop((324, 75, 368, 89))
+nome_img = img.crop((11, 210, 92, 230))
 
 nome = pytesseract.image_to_string(nome_img, lang="eng", config=r'--oem 3 --psm 7')
 valor = transforme_img_str(valor_img_conv)
@@ -308,7 +309,7 @@ if math.isnan(boleto_lista[0]) is not True:
     apertar('backspace')
     escreve('NFE {}'.format(numero_nfe))
     imagem_clicar('campo_valor_boleto.png')
-    escreve(valor)
+    escreve('{}'.format(valor))
     imagem_clicar('clicar_sacado.png')
     press_junto('alt', 'n')
     escreve(nome)
